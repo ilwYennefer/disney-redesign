2ae7941d74172397781fa0c8fc9a984e1af19b98
steve murphy <javierf7fywmrp@gmail.com>
2021-06-25 11:57:57 +0400

    created movie detail page

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -8,10 +8,17 @@ function Thumbnail({ result }) {
   return (
     <div
       className="rounded-xl overflow-hidden border-4 border-[#f9f9f9] border-opacity-10 shadow-image transform hover:scale-105 hover:border-opacity-80 hover:shadow-image-hover transition-all duration-250 ease-link-transition cursor-pointer"
+      // onClick={
+      //   result.media_type === "movie"
+      //     ? () => router.push(`/movie/${result.id}`)
+      //     : result.media_type === "tv"
+      //     ? () => router.push(`/tv/${result.id}`)
+      //     : ""
+      // }
       onClick={
-        result.media_type === "movie"
-          ? () => router.push(`/movie/${result.id}`)
-          : () => router.push(`/tv/${result.id}`)
+        result.media_type === "tv" || result.number_of_seasons
+          ? () => router.push(`/tv/${result.id}`)
+          : () => router.push(`/movie/${result.id}`)
       }
     >
       <Image
--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,5 +1,37 @@
-function Movie() {
-  return <div>Movie</div>;
+import Image from "next/image";
+
+function Movie({ movie }) {
+  console.log(movie);
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
+
+  return (
+    <div>
+      <h2>{movie.title || movie.original_name}</h2>
+      <Image
+        src={
+          `${BASE_URL}${movie.backdrop_path || movie.poster_path}` ||
+          `${BASE_URL}${movie.poster_path}`
+        }
+        layout="responsive"
+        height={1000}
+        width={1920}
+      />
+    </div>
+  );
 }
 
 export default Movie;
+
+export async function getServerSideProps(context) {
+  const id = context.query.id;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&language=en-US`
+  ).then((response) => response.json());
+
+  return {
+    props: {
+      movie: request,
+    },
+  };
+}
